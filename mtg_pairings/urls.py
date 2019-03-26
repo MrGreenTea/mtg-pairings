cca8eaf3a9a138b826b8e69541b4fb136a312034
MrGreenTea <mrlordalfred@gmail.com>
2019-03-26 17:05:03 +0100

    more design work

--- mtg_pairings/urls.py
+++ mtg_pairings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
     path('', views.ListTournaments.as_view(), name='tournament_list'),
+    path('start', views.CreateTournament.as_view(), name='create_tournament'),
     path('players/', views.ListPlayers.as_view(), name='player_list'),
     path('players/<str:pk>', views.ShowPlayer.as_view(), name='player_detail'),
     path('<int:pk>', views.ShowTournament.as_view(), name='tournament_detail')
