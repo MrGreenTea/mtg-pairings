9735294a137d3c9bbc696a5184d3a0e0c81450ea
MrGreenTea <mrlordalfred@gmail.com>
2019-03-26 19:56:30 +0100

    test settings

---
+++ MTG_Pairing/test_settings.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ.setdefault('DJANGO_SECRET_KEY', 'TEST_KEY')
+os.environ.setdefault('DJANGO_DEBUG', 'True')
+os.environ.setdefault('DJANGO_ALLOWED_HOSTS', '')
+os.environ.setdefault('DATABASE_URL', 'sqlite:////tmp/test_sqlite.db')
+
+from .settings import *
+
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+]
