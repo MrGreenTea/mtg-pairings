d21b9308967de48a9e2606d2222057f065238c3d
MrGreenTea <mrlordalfred@gmail.com>
2019-03-26 13:17:46 +0100

    add FREEWIN singleton Player object.

--- mtg_pairings/apps.py
+++ mtg_pairings/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from django.db import models
 
 
 class MtgPairingsConfig(AppConfig):
     name = 'mtg_pairings'
+
+    def __init__(self, app_name, app_module):
+        models.signals.class_prepared.connect(
+            receiver=self.init_freewin_player
+        )
+        super().__init__(app_name, app_module)
+
+    @staticmethod
+    def init_freewin_player(sender, **kwargs):
+        if hasattr(sender, "FREEWIN"):
+            print(sender, "FREEWIN set")
+            sender.FREEWIN, created = sender.objects.get_or_create(name="FREE WIN")
+            print("Created: ", created)
