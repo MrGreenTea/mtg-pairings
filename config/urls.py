6b9ac763154e47528d39cc9d5312544b7939a1c4
MrGreenTea <mrlordalfred@gmail.com>
2019-03-27 14:44:10 +0100

    move config.

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""MTG_Pairing URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('mtg_pairings.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('mtg_pairings.urls')),
+    # path('admin/', admin.site.urls),
 ]
+
