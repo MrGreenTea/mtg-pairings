39ae660ed8b3cdb77ab95d1d82b716acd4087fc2
MrGreenTea <mrlordalfred@gmail.com>
2019-03-27 17:33:57 +0100

    registering works now

--- mtg_pairings/accounts/forms.py
+++ mtg_pairings/accounts/forms.py
@@ -1,8 +1,10 @@
+from functools import partial
+
 import crispy_forms.helper
 from crispy_forms import layout
-from django.contrib.auth.forms import AuthenticationForm
 from django import forms
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 
 class LoginForm(AuthenticationForm):
@@ -12,14 +14,36 @@ class LoginForm(AuthenticationForm):
         self.helper.add_input(layout.Submit('submit', 'Submit'))
 
 
-class RegisterForm(LoginForm):
-    password_confirm = forms.CharField(
-        label=_("Confirm Password"),
-        strip=False,
-        widget=forms.PasswordInput,
-    )
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.attrs = {"class": "form-control"}
-
+        self.helper = crispy_forms.helper.FormHelper()
+        self.helper.attrs = {"class": "form-control"}
+        self.helper = crispy_forms.helper.FormHelper()
+        div = partial(layout.Div, css_class="form-group col-md-6")
+        self.helper.layout = layout.Layout(
+            layout.Row(
+                div("username"),
+                div("email"),
+                css_class="form-row",
+            ),
+            layout.Row(
+                div("first_name"),
+                div("last_name"),
+                css_class="form-row",
+            ),
+            layout.Row(
+                div("password1"),
+                div("password2"),
+                css_class="form-row",
+            ),
+            layout.Submit('submit', 'Submit'),
+        )
--- mtg_pairings/accounts/views.py
+++ mtg_pairings/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.contrib.auth.views import LoginView
 from django.views import generic
 
@@ -10,12 +11,21 @@ class ProfilePlayer(ShowPlayer):
         return self.request.user.player
 
 
-class RegisterUser(generic.FormView):
+class RegisterUser(generic.CreateView):
     form_class = mtg_pairings.accounts.forms.RegisterForm
     template_name = "registration/register.html"
 
-    def post(self, request, *args, **kwargs):
-        pass
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get("username")
+        raw_password = form.cleaned_data.get("password1")
+        user = auth.authenticate(username=username, password=raw_password)
+        auth.login(self.request, user)
+        return super(RegisterUser, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.object.player.get_absolute_url()
 
 
 class LoginUser(LoginView):
